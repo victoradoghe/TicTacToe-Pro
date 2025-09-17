84ea921ccba0d4f8267e7b9659dd0f3c165a0cda
victoradoghe <victoradoghe112@gmail.com>
2025-09-17 20:34:38 +0100

    bot Icon changed

--- src/App.jsx
+++ src/App.jsx
@@ -290,9 +290,49 @@ function App() {
                 onClick={() => startNewGame(GAME_MODES.TWO_PLAYER)}
               >
                 👥 Two Players
-              </button>
+              </button> 
               <div className="bot-mode">
-                <h3>🤖 Play vs Bot</h3>
+                <h3>
+                  <svg xmlns="http://www.w3.org/2000/svg" width="26px" height="26px" viewBox="0 0 20 20" style={{verticalAlign: 'middle', marginRight: 8}}>
+                    <g fill="none">
+                      <path fill="url(#SVGUOxJqe7I)" d="M10.25 17.998c2.616-.033 4.195-.595 5.122-1.44c.875-.8 1.089-1.777 1.123-2.556h.005v-.69a1.81 1.81 0 0 0-1.81-1.809H11.5V11.5h-3v.003H5.31c-1 0-1.81.81-1.81 1.81v.689h.005c.034.78.248 1.757 1.123 2.555c.927.846 2.506 1.408 5.122 1.441V18h.5z"></path>
+                      <path fill="url(#SVGxQ4fKeON)" d="M10.25 17.998c2.616-.033 4.195-.595 5.122-1.44c.875-.8 1.089-1.777 1.123-2.556h.005v-.69a1.81 1.81 0 0 0-1.81-1.809H11.5V11.5h-3v.003H5.31c-1 0-1.81.81-1.81 1.81v.689h.005c.034.78.248 1.757 1.123 2.555c.927.846 2.506 1.408 5.122 1.441V18h.5z"></path>
+                      <path fill="url(#SVGbzhGAd0I)" d="M10.5 2.5a.5.5 0 0 0-1 0v1h1z"></path>
+                      <path fill="url(#SVGmMrXkefk)" d="M5 4.5A1.5 1.5 0 0 1 6.5 3h7A1.5 1.5 0 0 1 15 4.5v4a1.5 1.5 0 0 1-1.5 1.5h-7A1.5 1.5 0 0 1 5 8.5z"></path>
+                      <path fill="url(#SVGspsrdbyg)" d="M12 5.5a1 1 0 1 0 0 2a1 1 0 0 0 0-2"></path>
+                      <path fill="url(#SVGnjJdNeec)" d="M7 6.5a1 1 0 1 1 2 0a1 1 0 0 1-2 0"></path>
+                      <defs>
+                        <linearGradient id="SVGxQ4fKeON" x1={10} x2={12.941} y1={10.726} y2={21.714} gradientUnits="userSpaceOnUse">
+                          <stop stopColor="#885edb" stopOpacity={0}></stop>
+                          <stop offset={1} stopColor="#e362f8"></stop>
+                        </linearGradient>
+                        <linearGradient id="SVGbzhGAd0I" x1={9.477} x2={10.532} y1={2} y2={3.229} gradientUnits="userSpaceOnUse">
+                          <stop stopColor="#8b52f4"></stop>
+                          <stop offset={1} stopColor="#3d35b1"></stop>
+                        </linearGradient>
+                        <linearGradient id="SVGspsrdbyg" x1={11.474} x2={12.99} y1={5.577} y2={8.193} gradientUnits="userSpaceOnUse">
+                          <stop stopColor="#fdfdfd"></stop>
+                          <stop offset={1} stopColor="#f9dcfa"></stop>
+                        </linearGradient>
+                        <linearGradient id="SVGnjJdNeec" x1={7.474} x2={8.99} y1={5.577} y2={8.193} gradientUnits="userSpaceOnUse">
+                          <stop stopColor="#fdfdfd"></stop>
+                          <stop offset={1} stopColor="#f9dcfa"></stop>
+                        </linearGradient>
+                        <radialGradient id="SVGUOxJqe7I" cx={0} cy={0} r={1} gradientTransform="matrix(15.59372 9.14999 -14.57328 24.83628 .5 9.5)" gradientUnits="userSpaceOnUse">
+                          <stop stopColor="#f08af4"></stop>
+                          <stop offset={0.535} stopColor="#9c6cfe"></stop>
+                          <stop offset={1} stopColor="#4e44db"></stop>
+                        </radialGradient>
+                        <radialGradient id="SVGmMrXkefk" cx={0} cy={0} r={1} gradientTransform="rotate(39.055 .75 2.115)scale(16.9824 32.4423)" gradientUnits="userSpaceOnUse">
+                          <stop stopColor="#f08af4"></stop>
+                          <stop offset={0.535} stopColor="#9c6cfe"></stop>
+                          <stop offset={1} stopColor="#4e44db"></stop>
+                        </radialGradient>
+                      </defs>
+                    </g>
+                  </svg>
+                  Play vs Bot
+                </h3>
                 <div className="difficulty-buttons">
                   <button 
                     className="difficulty-button easy" 
